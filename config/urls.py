36344b66e481eb4f963c53d1eb5989a3e2069ce7
KIM SEUNG YEON <winpond91@gmail.com>
2017-12-21 14:58:47 +0900

    Creating the URLS and testing the Image Serializers

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
 
     # User management
     url(r'^users/', include('yeongram.users.urls', namespace='users')),
+    url(r'^images/', include('yeongram.images.urls', namespace='images')),
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
--- yeongram/images/views.py
+++ yeongram/images/views.py
@@ -8,7 +8,7 @@ class ListAllImages(APIView):
     def get(self, request, format=None):
 
         all_images = models.Image.objects.all()
-
+        # 같은 시리얼라이저로 시리얼라이즈한다
         serializer = serializers.ImageSerializer(all_images, many=True)
 
         return Response(data=serializer.data)
