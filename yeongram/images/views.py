b5545eb6cc5ae716d1b1529e60dda98fc5d78c21
KIM SEUNG YEON <winpond91@gmail.com>
2017-12-21 17:46:37 +0900

    Getting the User Feed Part One

--- yeongram/images/views.py
+++ yeongram/images/views.py
@@ -2,40 +2,28 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
 
-# 요청의 접근 방식은 view functions의 두 번째 (명확하게는 첫 번째) attribute를 확인하면됨
-# 페이지가 요청될 때, 장고는 해당 요청에 대한 메타데이터를 포함한 http request를 생성한다
-class ListAllImages(APIView):
-    # 여기서 첫 번째 argument(=request)를 view function
-    def get(self, request, format=None):
-
-        print(request.scheme)
-
-        all_images = models.Image.objects.all()
-        # 같은 시리얼라이저로 시리얼라이즈한다
-        serializer = serializers.ImageSerializer(all_images, many=True)
-
-        return Response(data=serializer.data)
-
-
-class ListAllCommnets(APIView):
+class Feed(APIView):
 
     def get(self, request, format=None):
 
-        user_id = request.user.id
+        user = request.user
 
-        all_comments = models.Comment.objects.filter(creator=user_id)
+        following_users = user.following.all()
 
-        serializer = serializers.CommentSerializer(all_comments, many=True)
+        image_list = []
 
-        return Response(data=serializer.data)
+        for following_user in following_users:
 
+            user_images = following_user.images.all()[:2]
 
-class ListAllLikes(APIView):
+            for image in user_images:
 
-    def get(self, request, format=None):
+                image_list.append(image)
 
-        all_likes = models.Like.objects.all()
+        sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
 
-        serializer = serializers.LikeSerializer(all_likes, many=True)
+        print(sorted_list)
 
-        return Response(data=serializer.data)
+        serializer = serializers.ImageSerializer(sorted_list, many=True)
+
+        return Response(serializer.data)
