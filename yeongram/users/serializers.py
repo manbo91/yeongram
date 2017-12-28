4266340eae1342ddf7c8c5e89fe1afb77a3bef14
KIM SEUNG YEON <winpond91@gmail.com>
2017-12-28 17:15:17 +0900

    Update profile

--- yeongram/users/serializers.py
+++ yeongram/users/serializers.py
@@ -6,10 +6,14 @@ from yeongram.images import serializers as images_serializers
 class UserProfileSerializer(serializers.ModelSerializer):
 
     images = images_serializers.CountImageSerializer(many=True)
+    post_count = serializers.ReadOnlyField() # 해당 필드를 수정하지 않겠다는 것
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     class Meta:
         model = models.User
         fields = (
+            'profile_image',
             'username',
             'name',
             'bio',
