d30a44fa246a4d277113a7286a3c2fbe4be27cfa
KIM SEUNG YEON <winpond91@gmail.com>
2018-01-03 01:26:35 +0900

    React Design Patterns

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -9,3 +9,24 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(mapStateToProps)(Container);
+
+/*
+  -- React Design Patterns --
+  Container + Presenter Pattern
+
+  Call an API to get the latest blog posts.
+  Show a 'loading' status in the meantime.
+  When the call to the API is resolved, render the blog posts.
+
+  Approach One
+  without Container + Presenter Pattern
+
+  Approach Two
+  Container + Presenter Pattern
+
+  Container
+  Logic: API Requests, errors etc...
+
+  Presenter
+  Data comes from props. No logic. Only UI
+*/
