d4e31863a4ea5cf1c2a4e6b4502b1d0c00b150c2
KIM SEUNG YEON <winpond91@gmail.com>
2018-01-02 23:53:25 +0900

    Installing Redux Dev Tools

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import users from 'redux/modules/users';
 import Reactotron from 'ReactotronConfig';
 
@@ -25,7 +26,10 @@ let store;
 
 if (env === 'development') {
   store = initialState =>
-    Reactotron.createStore(reducer, applyMiddleware(...middlewares));
+    Reactotron.createStore(
+      reducer,
+      composeWithDevTools(applyMiddleware(...middlewares))
+    );
 } else {
   store = initialState =>
     createStore(reducer, applyMiddleware(...middlewares));
