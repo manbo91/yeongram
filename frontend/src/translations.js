431930b2e13bbb0e707af82c262f7c52d9f9b923
KIM SEUNG YEON <winpond91@gmail.com>
2018-01-04 10:59:55 +0900

    Stateless Components and Context

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,8 +1,19 @@
 export const translations = {
   es: {
-    "Login in": "Iniciar Serea"
+    "Login in": "로그인",
+    "About Us": "우리에 대해서",
   },
   kr: {
-    "Login in": "로그인"
+    "Login in": "로그인",
+    "About Us": "회사 소개",
+    "Support": "지원",
+    "Blog": "블로그",
+    "Press": "신문",
+    "API": "API",
+    "Jobs": "채용 정보",
+    "Privacy": "개인 정보",
+    "Terms": "이용 약관",
+    "Directory": "규칙",
+    "Language": "언어",
   }
 }
