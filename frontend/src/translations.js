8844d79641e762ea839dd9a9e48a3b9396479bc1
KIM SEUNG YEON <winpond91@gmail.com>
2018-01-04 11:10:04 +0900

    Importing Translations

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,19 +1,16 @@
 export const translations = {
-  es: {
-    "Login in": "로그인",
-    "About Us": "우리에 대해서",
+  'es': {
+    'About Us': 'asdasd',
+    'Support': 'qweqweqwe',
+    'Blog': '',
+    'Press': 'eqweqwe',
+    'API': '',
+    'Jobs': '',
+    'Privacy': '',
+    'Terms': '',
+    'Directory': '',
+    'Language': '',
   },
-  kr: {
-    "Login in": "로그인",
-    "About Us": "회사 소개",
-    "Support": "지원",
-    "Blog": "블로그",
-    "Press": "신문",
-    "API": "API",
-    "Jobs": "채용 정보",
-    "Privacy": "개인 정보",
-    "Terms": "이용 약관",
-    "Directory": "규칙",
-    "Language": "언어",
-  }
+  'options': {
+  },
 }
