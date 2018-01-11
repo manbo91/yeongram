b837f3d9d31e11890ad12868b0c52af7bb489ce5
KIM SEUNG YEON <winpond91@gmail.com>
2018-01-11 19:16:15 +0900

    FeedPhoto Component pt.Four

---
+++ frontend/src/components/CommentBox/container.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CommentBox from './presenter';
+
+const Container = props => <CommentBox {...props} />;
+
+export default Container;
