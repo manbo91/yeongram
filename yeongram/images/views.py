5e304cc69569bb0aa9418f92a780440d993dddc9
KIM SEUNG YEON <winpond91@gmail.com>
2018-01-11 21:10:01 +0900

    Detecting if an Image is Liked by me

--- yeongram/images/views.py
+++ yeongram/images/views.py
@@ -34,7 +34,7 @@ class Images(APIView):
 
         print(sorted_list)
 
-        serializer = serializers.ImageSerializer(sorted_list, many=True)
+        serializer = serializers.ImageSerializer(sorted_list, many=True, context={'request': request})
 
         return Response(serializer.data)
 
@@ -235,7 +235,7 @@ class ImageDetail(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)       
 
 
-        serializer = serializers.ImageSerializer(image)
+        serializer = serializers.ImageSerializer(image, context={'request': request})
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
