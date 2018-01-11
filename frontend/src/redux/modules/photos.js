a36bac865e9422860fb55a01a04c86979d1c4376
KIM SEUNG YEON <winpond91@gmail.com>
2018-01-11 21:29:26 +0900

    Liking Photos pt. One

--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -5,6 +5,8 @@ import { actionCreators as userActions } from "redux/modules/user";
 // actions
 
 const SET_FEED = "SET_FEED";
+const LIKE_PHOTO = "LIKE_PHOTO";
+const UNLIKE_PHOTO = "UNLIKE_PHOTO";
 
 // action creators
 
@@ -12,6 +14,20 @@ function setFeed(feed) {
   return {
     type: SET_FEED,
     feed
+  };
+}
+
+function doLikePhoto(photoId) {
+  return {
+    type: LIKE_PHOTO,
+    photoId
+  };
+}
+
+function doUnlikePhoto(photoId) {
+  return {
+    type: UNLIKE_PHOTO,
+    photoId
   };
 }
 
@@ -35,6 +51,13 @@ function getFeed() {
   };
 }
 
+function likePhoto(photoId) {
+  return (dispatch, getState) => {
+    dispatch(doLikePhoto(photoId));
+    fetch(`/images`);
+  };
+}
+
 // initial state
 
 const initialState = {};
@@ -45,6 +68,10 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_FEED:
       return applySetFeed(state, action);
+    case LIKE_PHOTO:
+      return applyLikePhoto(state, action);
+    case UNLIKE_PHOTO:
+      return applyUnlikePhoto(state, action);
     default:
       return state;
   }
@@ -58,6 +85,32 @@ function applySetFeed(state, action) {
     ...state,
     feed
   };
+}
+
+function applyLikePhoto(state, action) {
+  const { photoId } = action;
+  const { feed } = state;
+  const updatedFeed = feed.map(photo => {
+    if (photo.id === photoId) {
+      return { ...photo, is_liked: true, like_count: photo.like_count + 1 };
+    }
+    return photo;
+  });
+
+  return { ...state, feed: updatedFeed };
+}
+
+function applyUnlikePhoto(state, action) {
+  const { photoId } = action;
+  const { feed } = state;
+  const updatedFeed = feed.map(photo => {
+    if (photo.id === photoId) {
+      return { ...photo, is_liked: false, like_count: photo.like_count - 1 };
+    }
+    return photo;
+  });
+
+  return { ...state, feed: updatedFeed };  
 }
 
 // exports
